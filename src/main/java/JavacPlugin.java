5c4451df65d04f214a932eb9857252bf6514519d
Alexander Ackerman <alexanderackerman@Alexanders-MacBook-Pro-2.local>
2020-11-26 21:04:27 -0800

    back to working print statements

--- src/main/java/JavacPlugin.java
+++ src/main/java/JavacPlugin.java
@@ -17,36 +17,6 @@ public class JavacPlugin implements Plugin{
         return "MyPlugin";
     }
 
-    private void addLog(MethodTree method, Context context) {
-        TreeMaker factory = TreeMaker.instance(context);
-        Names names = Names.instance(context);
-        factory.at(((JCTree) method).pos); // set factory at method's position in the tree
-
-        // create the names used to access the method
-        Name n_system = names.fromString("System");
-        Name n_out = names.fromString("out");
-        Name n_print = names.fromString("println");
-
-        // create identifier for system
-        JCTree.JCIdent i_system = factory.Ident(n_system);
-        JCTree.JCFieldAccess i_sout = factory.Select(i_system, n_out);
-
-        // select the class and method call using the identifier and name?
-        JCTree.JCFieldAccess log_select = factory.Select(i_sout, n_print);
-
-        JCTree.JCMethodInvocation log_exp = factory.Apply(
-                com.sun.tools.javac.util.List.nil(), // might need to provide some type args?
-                log_select,
-                com.sun.tools.javac.util.List.of(factory.Literal("hello world! I was inserted by the plugin!"))
-        );
-        JCTree.JCStatement log_statement = factory.Exec(log_exp);
-        JCTree.JCBlock body = (JCTree.JCBlock) method.getBody();
-        if(body != null){
-            body.stats = body.stats.prepend(log_statement);
-        }
-
-    }
-
 
     public void init(JavacTask javacTask, String... strings) {
         Context context = ((BasicJavacTask) javacTask).getContext();
@@ -83,5 +53,35 @@ public class JavacPlugin implements Plugin{
                         null);
             }
         });
+    }
+
+    private void addLog(MethodTree method, Context context) {
+        TreeMaker factory = TreeMaker.instance(context);
+        Names names = Names.instance(context);
+        factory.at(((JCTree) method).pos); // set factory at method's position in the tree
+
+        // create the names used to access the method
+        Name n_system = names.fromString("System");
+        Name n_out = names.fromString("out");
+        Name n_print = names.fromString("println");
+
+        // create identifier for system
+        JCTree.JCIdent i_system = factory.Ident(n_system);
+        JCTree.JCFieldAccess i_sout = factory.Select(i_system, n_out);
+
+        // select the class and method call using the identifier and name?
+        JCTree.JCFieldAccess log_select = factory.Select(i_sout, n_print);
+
+        JCTree.JCMethodInvocation log_exp = factory.Apply(
+                com.sun.tools.javac.util.List.nil(), // might need to provide some type args?
+                log_select,
+                com.sun.tools.javac.util.List.of(factory.Literal("hello world! I was inserted by the plugin!"))
+        );
+        JCTree.JCStatement log_statement = factory.Exec(log_exp);
+        JCTree.JCBlock body = (JCTree.JCBlock) method.getBody();
+        if(body != null){
+            body.stats = body.stats.prepend(log_statement);
+        }
+
     }
 }
